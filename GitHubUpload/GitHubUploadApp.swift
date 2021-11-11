f427874ed3c696e037a674a1c40fe7a949bb3933
Marco <marco.chen@fullerton.com.tw>
2021-11-11 16:26:37 +0800

    Initial Commit

---
+++ GitHubUpload/GitHubUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUploadApp.swift
+//  GitHubUpload
+//
+//  Created by Marco on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
